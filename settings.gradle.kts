e1becb8eccd94bae14723379284249f41179ef8b
James Ward <james@jamesward.com>
2022-04-28 22:06:51 -0600

    cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,1 @@
-/*
-pluginManagement {
-    val quarkusPluginVersion: String by settings
-    val quarkusPluginId: String by settings
-    repositories {
-        mavenLocal()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-    plugins {
-        id(quarkusPluginId) version quarkusPluginVersion
-    }
-}
-
- */
 rootProject.name = "quarkus-kotlin-reactive-demo"
